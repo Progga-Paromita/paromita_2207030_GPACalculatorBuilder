d5e674d895850a3ca60826f200fd8da6474cffea
Progga Paromita <progga@example.com>
2025-11-16 23:16:19 +0600

    Your message

--- src/main/java/com/example/paromita_2207030_gpacalculatorbuilder/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.paromita_2207030_gpacalculatorbuilder;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
